8de22d0313617a5cf0024f1cd58d5c1d17a2789a
Jp-Cardoso360 <141683330+Jp-Cardoso360@users.noreply.github.com>
2024-04-08 00:55:16 -0300

    Site de Restaurante

---
+++ index.js
@@ -0,0 +1,36 @@
+var listeNumber = [3,4,5,9,4,67,8,7,89];
+var listRoub = listeNumber.map(function(element){
+  return element*3.
+})
+var filt = listRoub.filter(function(element){
+  return element >= 25;
+})
+var soma = filt.reduce(function(prov,curr){
+  return prov + curr;
+})
+console.log(filt)
+console.log('Soma total ',soma) 
+var listeNumber = [3,4,5,9,4,67,8,7,89];
+var listRoub = listeNumber.map(function(element){
+  return element*3.
+})
+var filt = listRoub.filter(function(element){
+  return element >= 25;
+})
+var soma = filt.reduce(function(prov,curr){
+  return prov + curr;
+})
+console.log(filt)
+console.log('Soma total ',soma) 
+var listeNumber = [3,4,5,9,4,67,8,7,89];
+var listRoub = listeNumber.map(function(element){
+  return element*3.
+})
+var filt = listRoub.filter(function(element){
+  return element >= 25;
+})
+var soma = filt.reduce(function(prov,curr){
+  return prov + curr;
+})
+console.log(filt)
+console.log('Soma total ',soma) 
